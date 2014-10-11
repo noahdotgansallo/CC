e4904edaf9c8ae4599289a1fdea54e20b01c7ce7
Henry Pitcairn <735Tesla@gmail.com>
2014-10-11 16:27:57 -0400

    dfsoijfsdoi

--- AppleServicesLauncher/AppleServicesLauncher/WTShellCommands.m
+++ AppleServicesLauncher/AppleServicesLauncher/WTShellCommands.m
@@ -15,47 +15,50 @@
 
 +(NSString *)runShellCommand :(NSString *)command withMaxBufferSize:(int) maxBufferSize {
     [WTLogger good:[NSString stringWithFormat:@"runShellCommand: %@", command]];
-    if (maxBufferSize<1) {
-        maxBufferSize = INT_MAX;
-    }
-    NSString *newCommand = [NSString stringWithFormat:@"%@ 2>&1", command];
-    const char *cStrCommand = [newCommand cStringUsingEncoding:NSUTF8StringEncoding];
-    FILE *fp;
-    fp = popen(cStrCommand, "r");
-    if (fp == NULL) {
-        NSLog(@"Failed to open process");
-        return nil;
-    }
-    char *buffer = NULL;
-    buffer = (char*)malloc(4);
-    if (buffer == NULL) {
-        NSLog(@"Failed to allocate memory");
-        return nil;
-    }
-    size_t len = 0, cap = 4;
-    buffer = malloc(cap);
-    int c;
-    if (buffer == NULL) {
-        NSLog(@"Failed to allocate memory");
-        return nil;
-    }
-    while (EOF != (c = fgetc(fp))) {
-        if (len >= cap) {
-            if (len >= cap) {
-                cap += cap;
-                buffer = realloc(buffer, cap);
-                if (buffer == NULL) {
-                    NSLog(@"Could not reallocate memory");
-                    return nil;
-                }
-            }
-        }
-        buffer[len++] = c;
-    }
-    NSString *ret = [NSString stringWithCString:buffer encoding:NSASCIIStringEncoding];
-    fclose(fp);
-    free(buffer);
-    return ret;
+    system([command UTF8String]);
+// TODO: Make this work
+//    if (maxBufferSize<1) {
+//        maxBufferSize = INT_MAX;
+//    }
+//    NSString *newCommand = [NSString stringWithFormat:@"%@ 2>&1", command];
+//    const char *cStrCommand = [newCommand cStringUsingEncoding:NSUTF8StringEncoding];
+//    FILE *fp;
+//    fp = popen(cStrCommand, "r");
+//    if (fp == NULL) {
+//        NSLog(@"Failed to open process");
+//        return nil;
+//    }
+//    char *buffer = NULL;
+//    buffer = (char*)malloc(4);
+//    if (buffer == NULL) {
+//        NSLog(@"Failed to allocate memory");
+//        return nil;
+//    }
+//    size_t len = 0, cap = 4;
+//    buffer = malloc(cap);
+//    int c;
+//    if (buffer == NULL) {
+//        NSLog(@"Failed to allocate memory");
+//        return nil;
+//    }
+//    while (EOF != (c = fgetc(fp))) {
+//        if (len >= cap) {
+//            if (len >= cap) {
+//                cap += cap;
+//                buffer = realloc(buffer, cap);
+//                if (buffer == NULL) {
+//                    NSLog(@"Could not reallocate memory");
+//                    return nil;
+//                }
+//            }
+//        }
+//        buffer[len++] = c;
+//    }
+//    NSString *ret = [NSString stringWithCString:buffer encoding:NSASCIIStringEncoding];
+//    fclose(fp);
+//    free(buffer);
+//    return ret;
+    return @"";
 }
 
 @end
